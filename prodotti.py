482f104e6d61970c024e6d608b785ad816d49ea5
DrLiuk <lucadrudi6@gmail.com>
2024-08-13 18:23:36 +0200

    Inizio lavoro

--- prodotti.py
+++ prodotti.py
@@ -1,25 +1,25 @@
 
 class Prodotto:
-    def __init__(self, cod, peso):
+    def __init__(self, cod, pesoTeorico):
        self.codice = cod
-       self.peso = peso
+       self.pesoTeorico = pesoTeorico
 
     def print(self):
        print(self.codice)
-       print(str(self.peso) + " Kg/m")
+       print(str(self.pesoTeorico) + " Kg/m")
     
     def to_kg(self,mt = 1):
-       return (self.peso * mt)
+       return (self.pesoTeorico * mt)
     
     def to_mt(self, kg = 1):
-       return (kg/self.peso)
+       return (kg/self.pesoTeorico)
 
 
 # CLASSE TUBOLARE
 class Tubolare(Prodotto):
-   def __init__(self, dim1, dim2, sp, peso):
+   def __init__(self, dim1, dim2, sp, pesoTeorico):
       cod = ("tb" + {dim1}+ "x" + {dim2} + "x" + {sp})
-      super().__init__(cod, peso)
+      super().__init__(cod, pesoTeorico)
       self.dim1 = dim1
       self.dim2 = dim2
       self.sp = sp
@@ -27,17 +27,17 @@ class Tubolare(Prodotto):
    def print(self):
       print("TUBOLARE")
       print({self.dim1} + "x" + {self.dim2} + "x" + {self.sp})
-      print("Peso: " + {self.peso})
+      print("Peso Teorico: " + {self.pesoTeorico})
     
 #CLASSE PIATTO LAMINATO
 class PiattoLam(Prodotto):
-   def __init__(self, dim1, sp, peso):
+   def __init__(self, dim1, sp, pesoTeorico):
       cod = ("ptLam" + {dim1}+ "x" + {sp})
-      super().__init__(cod, peso)
+      super().__init__(cod, pesoTeorico)
       self.dim1 = dim1
       self.sp = sp
    
    def print(self):
       print("PIATTO LAMINATO")
       print({self.dim1} + "x" + {self.sp})
-      print("Peso: " + {self.peso})
+      print("Peso Teorico: " + {self.pesoTeorico})
